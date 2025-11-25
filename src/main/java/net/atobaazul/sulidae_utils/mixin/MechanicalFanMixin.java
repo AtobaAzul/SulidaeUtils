9aaa4ff1ebe73ec945d603208eb370774b03a16f
AtobaAzul <atoba.azul.radical@gmail.com>
2025-11-24 21:27:00 -0300

    Fixed fans crashing when vertical & wasting tuyere durability.
    air ticks also now scale with RPM

--- src/main/java/net/atobaazul/sulidae_utils/mixin/MechanicalFanMixin.java
+++ src/main/java/net/atobaazul/sulidae_utils/mixin/MechanicalFanMixin.java
@@ -5,6 +5,8 @@ import com.simibubi.create.content.kinetics.fan.EncasedFanBlockEntity;
 import net.dries007.tfc.common.blocks.devices.IBellowsConsumer;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
@@ -17,7 +19,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(EncasedFanBlockEntity.class)
 public class MechanicalFanMixin extends KineticBlockEntity {
     @Unique
-    private static final int BELLOWS_AIR = 200;
+    private static final int BELLOWS_AIR = 100;
 
     public MechanicalFanMixin(BlockEntityType<?> typeIn, BlockPos pos, BlockState state) {
         super(typeIn, pos, state);
@@ -25,22 +27,26 @@ public class MechanicalFanMixin extends KineticBlockEntity {
 
     @Inject(method = "tick", at = @At("HEAD"), remap = false)
     private void sulidae_utils$tick(CallbackInfo ci) {
-        if (this.getSpeed() != 0) {
-
+        if (level != null && this.getSpeed() != 0) {
             final Direction direction = getBlockState().getValue(BlockStateProperties.FACING);
-            for (IBellowsConsumer.Offset offset : IBellowsConsumer.offsets()) {
 
-                final BlockPos airPosition = worldPosition.above(offset.up()).relative(direction, offset.out()).relative(direction.getClockWise(), offset.side());
-                final BlockState state = level.getBlockState(airPosition);
-                if (state.getBlock() instanceof IBellowsConsumer consumer) {
 
-                    if (consumer.canAcceptAir(level, airPosition, state)) {
+            if (!direction.getAxis().isVertical()) {
+                for (IBellowsConsumer.Offset offset : IBellowsConsumer.offsets()) {
+                    final BlockPos airPosition = worldPosition.above(offset.up()).relative(direction, offset.out()).relative(direction.getClockWise(), offset.side());
+                    final BlockState state = level.getBlockState(airPosition);
+                    BlockEntity be = level.getBlockEntity(airPosition);
 
-                        consumer.intakeAir(level, airPosition, state, BELLOWS_AIR);
+                    if (state.getBlock() instanceof IBellowsConsumer consumer && be != null) {
+                        CompoundTag nbt = be.serializeNBT();
+                        int airTicks = nbt.getInt("airTicks");
+
+                        if (consumer.canAcceptAir(level, airPosition, state) && airTicks <= 1) { //offset by 1 because wasn't properly hitting max temp.
+                            consumer.intakeAir(level, airPosition, state, (int) (BELLOWS_AIR + Math.abs(this.getSpeed())));
+                        }
                     }
                 }
             }
-
         }
     }
 }
